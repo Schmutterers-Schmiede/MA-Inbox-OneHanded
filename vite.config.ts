b1ddfd2cd0dcd39217d0525f91802889713c94f0
Philipp Schmutterer <trashmail837857@gmail.com>
2026-07-26 12:17:04 +0200

    correct vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: '/MA-Inbox-Right/',
+    base: '/MA-Inbox-OneHanded/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
